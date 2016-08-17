ac4b58b770404138487f283b0e2bbf51312d8bce
Gabi Volpe <gabriel.volpe@paddypower.com>
2016-08-17 14:26:08 +0100

    Exceptions

---
+++ exceptions/try-catch.hs
@@ -0,0 +1,17 @@
+import System.Environment  
+import System.IO  
+import System.IO.Error  
+  
+main = toTry `catch` handler  
+              
+toTry :: IO ()  
+toTry = do (fileName:_) <- getArgs  
+           contents <- readFile fileName  
+           putStrLn $ "The file has " ++ show (length (lines contents)) ++ " lines!"  
+ 
+handler :: IOError -> IO ()     
+handler e     
+    | isDoesNotExistError e =   
+        case ioeGetFileName e of Just path -> putStrLn $ "Whoops! File does not exist at: " ++ path  
+                                 Nothing -> putStrLn "Whoops! File does not exist at unknown location!"  
+    | otherwise = ioError e 
